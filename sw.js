3d50d6b70162007c32a49370c687cdcee95d3bdd
mohammedmo85 <34661862+mohammedmo85@users.noreply.github.com>
2026-06-05 11:38:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "noc-v1";
+const FILES = [
+  "/NOC-CALCULATOR/",
+  "/NOC-CALCULATOR/index.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
